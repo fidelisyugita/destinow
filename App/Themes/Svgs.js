a607e69316744977876e5e4c30c7d39442d823cf
Fidelis Yugita <fb46us@gmail.com>
2021-02-27 23:10:10 +0700

    remove and change unused data

---
+++ App/Themes/Svgs.js
@@ -0,0 +1,7 @@
+import Onboarding1 from '../Svgs/Onboarding1.svg';
+
+const svgs = {
+  Onboarding1: Onboarding1,
+};
+
+export default svgs;
--- App/Themes/index.js
+++ App/Themes/index.js
@@ -1,7 +1,8 @@
-import Colors from './Colors'
-import Fonts from './Fonts'
-import Metrics from './Metrics'
-import Images from './Images'
-import ApplicationStyles from './ApplicationStyles'
+import Colors from './Colors';
+import Fonts from './Fonts';
+import Metrics from './Metrics';
+import Images from './Images';
+import Svgs from './Svgs';
+import AppStyles from './AppStyles';
 
-export { Colors, Fonts, Images, Metrics, ApplicationStyles }
+export {Colors, Fonts, Metrics, Images, Svgs, AppStyles};
